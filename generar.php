bf5c5026a77408c16720bd873a17ccbd44654281
Civan98 <52634607+Civan98@users.noreply.github.com>
2020-09-16 15:33:15 -0500

    rediseño
    Se rediseño la cabezera

--- generar.php
+++ generar.php
@@ -7,15 +7,13 @@
     }
 
     function GenerarFolio(){
-        $dia = date("d");
-        $dia--;
 
         for ($i = 1; $i <= 2; $i++) {
             $folio1 = rand(0,9);
             $folio2 = rand(0,9);
         }
 
-        $folio = $dia.date("m").date("o").$folio1.$folio2;
+        $folio = date("d").date("m").date("o").$folio1.$folio2;
         echo "Folio ".$folio."<br>";
     }
 
