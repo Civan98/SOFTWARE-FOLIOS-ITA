831a5a0f250f0d133790fec6a052a77fdc5b551b
Ivan Catañeda Contreras <52634607+Civan98@users.noreply.github.com>
2020-11-17 21:48:25 -0600

    consulta para la contra del admin

--- modUser.php
+++ modUser.php
@@ -37,8 +37,13 @@ $depa = "";
 foreach ($buscarDeptoAS as $DaS) {
     $depa = $DaS['id_depto'];
 }
+if($nombreUsuario2 == 'admin'){
+    $actualizar = "UPDATE usuarios SET  contrasena = '$contrasena' WHERE nombreUsuario = '$nombreUsuario2' ";
+}else{
+    $actualizar = "UPDATE usuarios SET nombre = '$nombre', apellidos = '$apellidos', nombreUsuario = '$nombreUsuario', cargo = '$cargo', contrasena = '$contrasena', id_depto = '$depa', admin ='$admin', autoAutorizar = '$autoAutorizar'  WHERE nombreUsuario = '$nombreUsuario2' ";
+}
 
-$actualizar = "UPDATE usuarios SET nombre = '$nombre', apellidos = '$apellidos', nombreUsuario = '$nombreUsuario', cargo = '$cargo', contrasena = '$contrasena', id_depto = '$depa', admin ='$admin', autoAutorizar = '$autoAutorizar'  WHERE nombreUsuario = '$nombreUsuario2 ' ";
+
 $exec       = mysqli_query($conexion, $actualizar);
 
 if (!$exec) {
